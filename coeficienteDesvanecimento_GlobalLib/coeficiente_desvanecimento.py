1d62bb3fe7c2c1fad0f6f87dcd003903d21d9a7b
Yuripds <yurisantosypds@gmail.com>
2021-09-28 10:29:12 -0300

    justar as funções em um único arquivo

--- coeficienteDesvanecimento_GlobalLib/coeficiente_desvanecimento.py
+++ coeficienteDesvanecimento_GlobalLib/coeficiente_desvanecimento.py
@@ -1,17 +1,59 @@
 import numpy as np
 import math
 import random
-from coeficienteDevanecimento_GlobalLib import fading_doppler
-
 
 class Coeficiente_de_Desvanecimento:
+
+    def gcanal(self, NN, SEED):
+
+        np.random.seed(SEED)
+
+        RAND_MAX = 32767.0
+        fdmax = 10.0
+        fs = 10.0**4
+        ud = np.zeros((1, NN))
+        ua = np.zeros((1, NN))
+
+        for j in range(NN):
+            ud[:, j] = np.random.randint(0, RAND_MAX, size=1)/RAND_MAX
+            ua[:, j] = np.random.randint(0, RAND_MAX, size=1)/RAND_MAX
+
+        dn = np.zeros((1, NN))
+        an = np.zeros((1, NN), dtype=complex)
+        for j in range(NN):
+            dn[:, j] = fdmax*np.cos(2.0*np.pi*ud[:, j])
+            rel = np.cos(2.0*np.pi*ua[:, j])
+            img = np.sin(2.0*np.pi*ua[:, j])
+            an[:, j] = rel + 1j*img
+        T = 1.0/fs
+        doppler = np.zeros((1, NN))
+        for j in range(NN):
+            doppler[:, j] = 2.0*np.pi*T*dn[:, j]
+        return an, doppler
+
+    def gera_canal(self, NN, conc, an, doppler):
+
+        e = np.zeros((1, NN), dtype=complex)
+        h = np.zeros((1, NN), dtype=complex)
+        h_n = np.zeros((1, 1), dtype=complex)
+
+        for j in range(NN):
+            rel = np.cos(conc*doppler[:, j])
+            img = np.sin(conc*doppler[:, j])
+            e[:, j] = rel+1j*img
+            h[:, j] = an[:, j]*e[:, j]
+
+        h_n = np.sum(h)
+        h_n = (1.0/np.sqrt(NN))*h_n
+
+        return h_n
 
     def fading(self,tamanho, seed):
         h = np.zeros((1, tamanho), dtype=complex)
         conc = 1.0
-        an, doppler = fading_doppler.Fading_Doppler.gcanal(self.NN, seed)
+        an, doppler = self.gcanal(self.NN, seed)
         for j in range(tamanho):
-            h[:, j] = fading_doppler.Fading_Doppler.gera_canal(self.NN, conc, an, doppler)
+            h[:, j] = self.gera_canal(self.NN, conc, an, doppler)
             conc = conc+1.0
         return h
 
