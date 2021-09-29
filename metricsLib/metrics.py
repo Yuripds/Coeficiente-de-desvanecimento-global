117d29e7d6a64da12ef4dc2a7df6820aa73a2ba9
Yuripds <yurisantosypds@gmail.com>
2021-09-29 10:35:39 -0300

    correção de código

--- metricsLib/metrics.py
+++ metricsLib/metrics.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class Metrics:
 
-    def data_rate_r1(w,B,p1,gama1,N0):
+    def data_rate_r1(self,w,B,p1,gama1,N0):
         num = p1*gama1
 
         den = (w*N0*B) 
@@ -12,7 +12,7 @@ class Metrics:
 
         return r1
 
-    def data_rate_r2(w,B,p2,gama2,p1,N0):
+    def data_rate_r2(self,w,B,p2,gama2,p1,N0):
         num = p2*gama2
 
         den = (p1*gama2)+(w*N0*B) 
@@ -22,7 +22,7 @@ class Metrics:
         return r2
 
 
-    def data_rate_r3(w,B,p3,gama3,p1,p2,N0):
+    def data_rate_r3(self,w,B,p3,gama3,p1,p2,N0):
         num = p3*gama3
 
         den = (p1*gama3)+(p2*gama3)+(w*N0*B) 
@@ -31,7 +31,7 @@ class Metrics:
 
         return r3
 
-    def data_rate_r4(w,B,p4,gama4,p1,p2,p3,N0):
+    def data_rate_r4(self,w,B,p4,gama4,p1,p2,p3,N0):
         num = p4*gama4
 
         den = (p1*gama4)+(p2*gama4)+(p3*gama4)+(w*N0*B)  
@@ -40,7 +40,7 @@ class Metrics:
 
         return r4
 
-    def data_rate_r5(w,B,p5,gama5,p1,p2,p3,p4,N0):
+    def data_rate_r5(self,w,B,p5,gama5,p1,p2,p3,p4,N0):
         num = p5*gama5
 
         den = (p1*gama5)+(p2*gama5)+(p3*gama5)+(p4*gama5)+(w*N0*B) 
@@ -49,7 +49,7 @@ class Metrics:
 
         return r5
 
-    def data_rate_r6(w,B,p6,gama6,p1,p2,p3,p4,p5,N0):  
+    def data_rate_r6(self,w,B,p6,gama6,p1,p2,p3,p4,p5,N0):  
         num = p6*gama6
 
         den = (p1*gama6)+(p2*gama6)+(p3*gama6)+(p4*gama6)+(p5*gama6)+ (w*N0*B)
